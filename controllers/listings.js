46627aa3badad35c274d8ae9fb62755e2fd52ff6
Vinay Kumar <vy2630066@gmail.com>
2025-09-03 13:28:12 +0530

    change listing.js update route

--- controllers/listings.js
+++ controllers/listings.js
@@ -75,7 +75,7 @@ module.exports.updateListing = async (req, res) => {
     
     let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
 
-    if(typeof req.file !== "undefined") {
+    if(req.file) {
         let url = req.file.path;
         let filename = req.file.filename;
 

25e2087e9a2d62842ed4e16726e38fcb6e373edb
Vinay Kumar <vy2630066@gmail.com>
2025-09-23 22:25:42 +0530

    add new features

--- models/listing.js
+++ models/listing.js
@@ -14,11 +14,6 @@ const listingSchema = new Schema({
     },
     
     image: {
-        // url: {
-        //     type: String,
-        //     default: "https://images.unsplash.com/photo-1745685088912-85233c4f16ac?q=80&w=1247&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        //     set: (v) => v===""?"https://images.unsplash.com/photo-1745685088912-85233c4f16ac?q=80&w=1247&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-        // },
         url: String,
         filename: String,
     },
